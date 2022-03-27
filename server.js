6318bcea7c343c983e5b115b6678420d34081471
Maria <maria@C02PRSU7G8WP.local>
2022-03-27 19:10:34 +0100

    added server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+
+// const inventoryRoutes = require("./routes/inventoryRoutes");
+// const warehouseRoutes = require("./routes/warehouseRoutes");
+
+const app = express();
+const PORT = 8080 || 4040;
+
+app.use(express.json());
+
+app.use(cors());
+
+// app.use(warehouseRoutes);
+// app.use(inventoryRoutes);
+
+app.get("/", (_req, res) => {
+  res.send("Welcome to the Mood Tracker Server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Mood Tracker Server listening at http://localhost:${PORT}`);
+});
