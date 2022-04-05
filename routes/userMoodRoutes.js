573d47f8c49e6b0d9d8a91695546bd3bb4c4eb68
Maria <mariiamanger@gmail.com>
2022-04-06 00:36:51 +0100

    database added

--- routes/userMoodRoutes.js
+++ routes/userMoodRoutes.js
@@ -1,9 +1,8 @@
 const router = require("express").Router();
-const fs = require("fs");
 const userMoodControl = require("../controllers/userMoodControl");
 
 // User mood tracker routes
-router.get("/mood-user", userMoodControl.getUsermood);
+router.get("/mood-user", userMoodControl.getUserMoods);
 router.post("/mood-user", userMoodControl.addNewMood);
 router.put("/mood-user/:id", userMoodControl.editMood);
 
